738ab14ff3319a54802cbc324cc2f93aa9ea138f
akarshkathal <akarshkathal@gmail.com>
2024-05-15 15:07:33 -0700

    Updating account.component.spec.ts for test case modification

--- src/app/account/account.component.spec.ts
+++ src/app/account/account.component.spec.ts
@@ -1,22 +1,23 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-
 import { AccountComponent } from "./account.component";
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe("AccountComponent", () => {
-	let component: AccountComponent;
-	let fixture: ComponentFixture<AccountComponent>;
+  let component: AccountComponent;
+  let fixture: ComponentFixture<AccountComponent>;
 
-	beforeEach(async () => {
-		await TestBed.configureTestingModule({
-			declarations: [AccountComponent],
-		}).compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AccountComponent],
+      imports: [ReactiveFormsModule] // Add this line
+    }).compileComponents();
 
-		fixture = TestBed.createComponent(AccountComponent);
-		component = fixture.componentInstance;
-		fixture.detectChanges();
-	});
+    fixture = TestBed.createComponent(AccountComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-	it("should create", () => {
-		expect(component).toBeTruthy();
-	});
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
 });
