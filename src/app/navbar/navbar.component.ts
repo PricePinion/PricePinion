82bf5d2d42f83020ba0629e996dcc4d9fe42c809
akarshkathal <akarshkathal@gmail.com>
2024-06-04 11:56:46 -0700

    Updating navbar.component.ts

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../authService';
 
 @Component({
-	selector: "app-navbar",
-	templateUrl: "./navbar.component.html",
-	styleUrl: "./navbar.component.css",
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  isLoggedIn: boolean = false;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.authService.user.subscribe(user => {
+      this.isLoggedIn = !!user;
+    });
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
+
+  authenticateGoogle(): void {
+    this.authService.login();
+  }
+}
